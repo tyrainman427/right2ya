9271ff3628ed89b0b2503bd336a95ce72fc5a492
tyrainman427 <tyraineytech@gmail.com>
2023-04-20 01:58:34 -0400

    fixed real time changes to customer

--- core/consumers.py
+++ core/consumers.py
@@ -7,46 +7,73 @@ class JobConsumer(WebsocketConsumer):
   def connect(self):
     self.job_id = self.scope['url_route']['kwargs']['job_id']
     self.job_group_name = 'job_%s' % self.job_id
+    self.job = models.Job.objects.get(id=self.job_id)
 
     # Join room group
     async_to_sync(self.channel_layer.group_add)(
       self.job_group_name,
       self.channel_name
     )
-
+    print("Joining room")
     self.accept()
-
-  def disconnect(self, close_code):
-    # Leave room group
-    async_to_sync(self.channel_layer.group_discard)(
-      self.job_group_name,
-      self.channel_name
-    )
-
+    
   def receive(self, text_data):
-    text_data_json = json.loads(text_data)
-    job = text_data_json['job']
+    print("sending message to the group")
+    data = json.loads(text_data)
+    job = data.get('job')
 
-    # print("Job", job)
+    if job:
+        courier = self.scope['user'].courier
+        if 'courier_lat' in job:
+            courier.lat = job['courier_lat']
+        if 'courier_lng' in job:
+            courier.lng = job['courier_lng']
+        courier.save()
+        job['courier'] = {'lat': courier.lat, 'lng': courier.lng}
 
-    if job.get('courier_lat') and job.get('courier_lng'):
-      self.scope['user'].courier.lat = job['courier_lat']
-      self.scope['user'].courier.lng = job['courier_lng']
-      self.scope['user'].courier.save()
+        # Send message to job group
+        async_to_sync(self.channel_layer.group_send)(
+            self.job_group_name,
+            {
+                'type': 'job_update',
+                'job': job,
+            }
+        )
+    else:
+        print('Invalid job data')
 
-    # Send message to job group
-    async_to_sync(self.channel_layer.group_send)(
-      self.job_group_name,
-      {
-        'type': 'job_update',
-        'job': job
-      }
-    )
+  # def receive(self, text_data):
+  #   text_data_json = json.loads(text_data)
+  #   job = text_data_json['job']
+  #   print("received job")
+  #   print("Job", job)
+
+  #   if job.get('courier_lat') and job.get('courier_lng'):
+  #     self.scope['user'].courier.lat = job['courier_lat']
+  #     self.scope['user'].courier.lng = job['courier_lng']
+  #     self.scope['user'].courier.save()
+
+  #   # Send message to job group
+  #   async_to_sync(self.channel_layer.group_send)(
+  #     self.job_group_name,
+  #     {
+  #       'type': 'job_update',
+  #       'job': job
+  #     }
+  #   )
+    
     
 
   # Receive message from job group
   def job_update(self, event):
-    job = event['job']
 
     # Send message to WebSocket
-    self.send(text_data=json.dumps(job))
+    self.send(text_data=json.dumps(event))
+    
+  def disconnect(self, close_code):
+        # Leave room group
+    async_to_sync(self.channel_layer.group_discard)(
+      self.job_group_name,
+      self.channel_name
+    )
+    print("Leaving room")
--- fastparcel/asgi.py
+++ fastparcel/asgi.py
@@ -1,22 +1,24 @@
 import os
-
+from django.urls import path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from core.consumers import JobConsumer
 from django.core.asgi import get_asgi_application
 import core.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastparcel.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastparcel.settings")
+# # Initialize Django ASGI application early to ensure the AppRegistry
+# # is populated before importing code that may import ORM models.
+# django_asgi_app = get_asgi_application()
 
 
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
-        "websocket": 
-            AuthMiddlewareStack(URLRouter(core.routing.websocket_urlpatterns))
-        ,
-    }
-)
+        # "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter([
+                path('ws/jobs/<job_id>/', JobConsumer.as_asgi()),
+            ])
+        ),
+    })
