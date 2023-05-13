421475f576a61f41f02cb8502b7e5c862ef1799f
tyrainman427 <tyraineytech@gmail.com>
2023-05-13 14:45:47 -0400

    signup,reset and login flow fixed

--- core/views.py
+++ core/views.py
@@ -15,11 +15,46 @@ from django.http import JsonResponse
 from datetime import datetime, timedelta
 from .models import Job
 from .serializers import JobSerializer
-
+from django.urls import reverse
 
 from . import forms
 from django.conf import settings
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.db.models.query_utils import Q
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 
+
+
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			associated_users = User.objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "registration/password_reset_email.txt"
+					c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Right2ya Beta',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'Right2ya Beta' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+					return redirect ("/password_reset/done/")
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="registration/password_reset.html", context={"password_reset_form":password_reset_form})
 
 def home(request):
     return render(request, 'home.html')
@@ -46,34 +81,69 @@ def home(request):
 #     })
 
 def sign_up(request):  
-    if request.method == 'POST':  
-        form = forms.SignUpForm(request.POST)  
-        
-        if form.is_valid():  
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST)
+
+        if form.is_valid():
             email = form.cleaned_data.get('email').lower()
-            # save form in the memory not in database  
-            user = form.save(commit=False)  
+            # save form in the memory not in database
+            user = form.save(commit=False)
             user.username = email
-            user.is_active = False  
-            user.save()  
-            # to get the domain of the current site  
-            current_site = get_current_site(request)  
-            mail_subject = 'Welcome to Right 2 Ya Beta!'  
-            message = render_to_string('acc_active_email.html', {  
-                'user': user,  
-                'domain': current_site.domain,  
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
-                'token':account_activation_token.make_token(user),  
-            })  
-            to_email = form.cleaned_data.get('email')  
-            email = EmailMessage(  
-                        mail_subject, message, to=[to_email]  
-            )  
-            email.send()  
-            return HttpResponse('Please confirm your email address to complete the registration')  
-    else:  
-        form = forms.SignUpForm()  
-    return render(request, 'sign_up.html', {'form': form})  
+            user.is_active = False
+            user.save()
+            # to get the domain of the current site
+            current_site = get_current_site(request)
+            mail_subject = 'Welcome to Right 2 Ya Beta!'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            activation_url = reverse('activate', kwargs={
+                'uidb64': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            full_activation_url = request.build_absolute_uri(activation_url)
+            print('Activation URL:', full_activation_url)
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = forms.SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
+
+    # if request.method == 'POST':  
+    #     form = forms.SignUpForm(request.POST)  
+        
+    #     if form.is_valid():  
+    #         email = form.cleaned_data.get('email').lower()
+    #         # save form in the memory not in database  
+    #         user = form.save(commit=False)  
+    #         user.username = email
+    #         user.is_active = False  
+    #         user.save()  
+    #         # to get the domain of the current site  
+    #         current_site = get_current_site(request)  
+    #         mail_subject = 'Welcome to Right 2 Ya Beta!'  
+    #         message = render_to_string('acc_active_email.html', {  
+    #             'user': user,  
+    #             'domain': current_site.domain,  
+    #             'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+    #             'token':account_activation_token.make_token(user),  
+    #         })  
+    #         to_email = form.cleaned_data.get('email')  
+    #         email = EmailMessage(  
+    #                     mail_subject, message, to=[to_email]  
+    #         )  
+    #         email.send()  
+    #         return HttpResponse('Please confirm your email address to complete the registration')  
+    # else:  
+    #     form = forms.SignUpForm()  
+    # return render(request, 'sign_up.html', {'form': form})  
 
 def activate(request, uidb64, token):  
     User = get_user_model()  
